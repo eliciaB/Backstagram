c1b41a12dfa21f65f1c101c7539114d193ffc85a
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-06-01 19:10:07 -0500

    added delete button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
+import ClearSharpIcon from '@material-ui/icons/ClearSharp';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const ListItem = ({name}) => {
     return (
         <div className="ListItemStyle">
             <Paper>
-                <div className="ListItemNameContainerStyle">
-                    {name}
-                </div>
+                <Grid container direction="row" justify="space-between" alignItems="center">
+                    <Grid item> 
+                        <div className="ListItemNameContainerStyle">
+                            {name}
+                        </div>
+                    </Grid>
+                    <Grid item> 
+                        <Button>
+                            <ClearSharpIcon></ClearSharpIcon> 
+                        </Button>
+                    </Grid>
+                </Grid>
             </Paper> 
         </div>
     )
