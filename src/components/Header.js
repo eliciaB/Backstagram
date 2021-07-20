5003a7459c9a882e660c11bc56592575b9bcb49c
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-07-20 16:10:37 -0500

    Added bike logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import logo from '../static/LimeBike-Logo.png';
+
+console.log(logo);
 
 const Header = (props) => {
     return ( 
         <div className="HeaderStyle">
+            <img style={{width: '80px', marginRight: '1300px', marginTop: '30px'}} src={logo} alt="Logo" />
             <marquee>
                 <h1>{props.title}</h1>
             </marquee>
