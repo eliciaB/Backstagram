1acbdc5473c4b831e1f18a090473300bb2b26c70
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-07-20 17:05:48 -0500

    initial slide effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import logo from '../static/LimeBike-Logo.png';
+import Slide from '@material-ui/core/Slide';
 
 console.log(logo);
 
 const Header = (props) => {
     return ( 
         <div className="HeaderStyle">
-            <img style={{width: '80px', marginRight: '1300px', marginTop: '30px'}} src={logo} alt="Logo" />
-            <marquee>
-                <h1>{props.title}</h1>
-            </marquee>
+            <Slide direction="left" timeout={7000} in={true} mountOnEnter unmountOnExit>
+               <div> 
+                    <img className="ImageStyleLogo" src={logo} alt="Logo" />
+                    <h1 className="HeaderTextStyle">{props.title}</h1>
+               </div>
+            </Slide> 
             <h2 className= "Style2">Do It.</h2>
         </div>
     )
