724b6180163885015a957d00d16dc1374adb28ec
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-08-03 19:02:07 -0500

    added the body to the InstaPost

--- src/components/InstaPost.js
+++ src/components/InstaPost.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import ShareIcon from '@material-ui/icons/Share'
+
 
 const InstaPost = () => {
     return (
-        <div>
-            lfkdsjje
-        </div>
+        <Card className="InstaPostCard"> 
+            <CardHeader
+                avatar={
+                    <Avatar>
+                    </Avatar>
+                }    
+                action={
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title="skljei"
+                subheader="fkljf"
+            />
+            {/* ToDo: put card media here */}
+
+            <CardContent>
+                OMGG feeling cuteee might delete later
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton>
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton>
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+
+
+        
     )
 }
 
