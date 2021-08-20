32097285606807a01eea8e2cfa610ecd61872755
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-08-20 18:04:10 -0500

    initial set up for instaPost

--- src/components/InstaPost.js
+++ src/components/InstaPost.js
@@ -8,39 +8,57 @@ import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import ShareIcon from '@material-ui/icons/Share'
+import CardMedia from '@material-ui/core/CardMedia'
+import {useState} from 'react'
 
 
 const InstaPost = () => {
+    const [imgSrc, setImgSrc]=useState("")
+
+    const upoloadImage =(imgFile) => {
+        console.log("uploadingImage")
+        console.log(imgFile)
+        
+        const tempImgSrc=URL.createObjectURL(imgFile)
+        setImgSrc(tempImgSrc)
+        console.log(tempImgSrc)
+    }    
+
     return (
-        <Card className="InstaPostCard"> 
-            <CardHeader
-                avatar={
-                    <Avatar>
-                    </Avatar>
-                }    
-                action={
+        <div>
+            <input type="file" onChange={(e)=>upoloadImage(e.target.files[0])}/>
+            <Card className="InstaPostCard"> 
+                <CardHeader
+                    avatar={
+                        <Avatar>
+                        </Avatar>
+                    }    
+                    action={
+                        <IconButton>
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title="skljei"
+                    subheader="fkljf"
+                />
+                <img className="InstaPostMediaStyle"
+                    src={imgSrc}
+                    alt=""
+                />
+
+                <CardContent>
+                    OMGG feeling cuteee might delete later
+                </CardContent>
+                <CardActions disableSpacing>
                     <IconButton>
-                        <MoreVertIcon />
+                        <FavoriteIcon />
                     </IconButton>
-                }
-                title="skljei"
-                subheader="fkljf"
-            />
-            {/* ToDo: put card media here */}
-
-            <CardContent>
-                OMGG feeling cuteee might delete later
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton>
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton>
-                    <ShareIcon />
-                </IconButton>
-            </CardActions>
-        </Card>
-
+                    <IconButton>
+                        <ShareIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </div>
 
         
     )
