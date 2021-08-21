f0a2570744fb6d5a3572d3865e3db00b6c2f4c5d
FlamminHotSyl <lemon.tree36777@gmail.com>
2021-08-20 19:50:43 -0500

    added instaPost form

--- src/components/InstaPostForm.js
+++ src/components/InstaPostForm.js
@@ -1,9 +1,27 @@
+import { Paper, TextField, Button, Grid } from '@material-ui/core'
 import React from 'react'
+
 
 const InstaPostForm = () => {
     return (
         <div>
-            lksjfjoiwe
+            <Paper className="InstaPostFormStyle">
+                <Grid container 
+                    alignItems="center" 
+                    direction="row"
+                    justify="center"
+                >
+                    <Grid item xs><TextField variant="outlined"/></Grid>
+                    <Grid item container xs
+                        alignItems="center" 
+                        direction="column"
+                        justify="center"
+                    >
+                        <Button> Add image </Button>
+                        <Button> Post </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
         </div>
     )
 }
